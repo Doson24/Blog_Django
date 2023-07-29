d6b1b6995f19e69051a75ac9ce3b627a6457af92
Doson <karbushev-1991@mail.ru>
2023-07-29 15:03:26 +0700

    создание модели Comment

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,6 +5,7 @@ from django.db import models
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -12,23 +13,21 @@ class PublishedManager(models.Manager):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 
 
-class HUI(models.Model):
-    pass
-
-
 class Post(models.Model):
     """
-    slug - короткая метка поста, соержит только буквы, цифры, знаки
-    подчеркивания или дефисы.
-    (Для формирования дружественной поисковой оптимизации адресов)
-    """
+            slug - короткая метка поста, соержит только буквы, цифры, знаки
+            подчеркивания или дефисы.
+            (Для формирования дружественной поисковой оптимизации адресов)
+            """
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PD', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250,
+                            unique_for_date='publish')
+
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -50,6 +49,35 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+
+    indexes = [
+        models.Index(fields=['created']),
+    ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
 
 
 """
